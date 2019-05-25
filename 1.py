69815067571e9bb0ecf2a144234a49a082d8ca9d
akshat-bhatia <36645756+akshat-bhatia@users.noreply.github.com>
2019-05-25 13:48:51 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Data.csv')
+
+#Dividing the dataset based on dependent and independent decision variable
+
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,3].values
+
+#Missing data handling
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='Nan', strategy='mean', axis=0)
+imputer = Imputer.fit(X)
+X[:, 1:3] = Imputer.transform(X[:, 1:3])
