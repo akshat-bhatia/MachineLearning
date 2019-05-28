9c8123f2289677a8be5f4f755795d434ddcffd0d
akshat-bhatia <36645756+akshat-bhatia@users.noreply.github.com>
2019-05-27 20:58:21 -0400

    Update DataPreprocessing.py

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -36,3 +36,7 @@ X = OneHotEncoder.fit_transform(X).toarray()
 
 labelencoder_Y = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
+
+#Splitting the dataset into training and testing dataset
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state = 0 )
